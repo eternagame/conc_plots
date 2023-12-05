40acc29e3cbf9dc0f5ece9d7b07fdb0b92037c1b
Jonathan Romano <jonathan@luxaritas.com>
2023-12-05 20:14:52 +0000

    Lower min python version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
 	name='conc-plots',
 	packages=['conc_plots'],
 	package_dir={'conc_plots': root_path},
-	python_requires='>3.10,<4',
+	python_requires='>3.7,<4',
 	install_requires=[
         'numpy>=1.14,<2',
         'pandas>=0.22.0,<3',
